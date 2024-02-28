95dbfda56c4f62846a5eefaa4ea8d67bb9a66c42
Eran Boodnero <eboodnero@gmail.com>
2024-02-27 17:09:23 -0800

    updated neovim

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -5,6 +5,43 @@
 vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
 vim.g.material_style = 'deep ocean'
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Saving with <C-s>
+map('n', '<C-s>', ':w<CR>', opts)
+
+-- Navigation between buffers
+map('n', '<S-l>', ':BufferLineCycleNext<CR>', opts)
+map('n', '<S-h>', ':BufferLineCyclePrev<CR>', opts)
+
+-- Scrolling with centering
+map('n', '<C-d>', '<C-d>zz', opts)
+map('n', '<C-u>', '<C-u>zz', opts)
+
+-- Moving lines in visual mode
+map('v', 'J', ":m '>+1<CR>gv=gv", opts)
+map('v', 'K', ":m '<-2<CR>gv=gv", opts)
+
+-- Telescope and Trouble
+map('n', '<leader><leader>', ':Telescope buffers<CR>', opts)
+map('n', '<leader>t', '<cmd>TroubleToggle<CR>', opts)
+map('n', '<leader>o', '<cmd>SymbolsOutline<CR>', opts)
+
+-- Tmux navigation
+map('n', '<C-h>', '<cmd> TmuxNavigateLeft<CR>', opts)
+map('n', '<C-l>', '<cmd> TmuxNavigateRight<CR>', opts)
+map('n', '<C-j>', '<cmd> TmuxNavigateDown<CR>', opts)
+map('n', '<C-k>', '<cmd> TmuxNavigateUp<CR>', opts)
+
+-- Meta key window navigation
+map('n', '<m-h>', '<C-w>h', opts)
+map('n', '<m-j>', '<C-w>j', opts)
+map('n', '<m-k>', '<C-w>k', opts)
+map('n', '<m-l>', '<C-w>l', opts)
+map('n', '<m-tab>', '<c-6>', opts)
+
 return {
   {
     'nvim-neo-tree/neo-tree.nvim',
